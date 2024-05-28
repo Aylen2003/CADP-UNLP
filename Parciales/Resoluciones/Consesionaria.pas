517bb741ad6db10658b1bcbf1395f13b52bc1053
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-05-28 12:20:38 -0300

    Create Consesionaria.pas

---
+++ Parciales/Resoluciones/Consesionaria.pas
@@ -0,0 +1,13 @@
+{Una concesionaria de la ciudad de La Plata necesita un programa para administrar
+información de ventas de autos efectuadas en febrero de 2020. Para ello, se debe leer la
+información de las ventas realizadas. De cada venta se lee: código de venta, modelo de
+auto, DNI del cliente al que se vendió el auto, día de la venta (1..28). La información de las
+ventas se lee de manera ordenada por modelo de auto y finaliza cuando se ingresa el
+modelo ‘ZZZ’ (que no debe procesarse).
+Se pide:
+A) Generar una estructura que contenga, para cada modelo de auto, la cantidad de veces
+que fue vendido. Esta estructura debe quedar ordenada por modelo de auto.
+B) Calcular e informar el día del mes en que se realizaron más ventas a clientes con DNI
+impar.
+C) Calcular e informar el porcentaje de ventas con código par.}
+
