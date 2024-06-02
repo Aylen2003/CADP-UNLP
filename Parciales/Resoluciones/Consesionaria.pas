844ffdf34a177a10146cba3c27bc25162e2e06b6
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-02 13:41:19 -0300

    Update Consesionaria.pas

--- Parciales/Resoluciones/Consesionaria.pas
+++ Parciales/Resoluciones/Consesionaria.pas
@@ -11,3 +11,99 @@ B) Calcular e informar el día del mes en que se realizaron más ventas a client
 impar.
 C) Calcular e informar el porcentaje de ventas con código par.}
 
+program Concesionaria;
+const
+ r = 28;
+type 
+ cadena = string[50];
+ rango = 1..r;
+ ventas = record
+   codigo,dni: integer;
+   auto: cadena;
+   dia: rango;
+ end;
+ lista = ^nodo;
+ nodo = record
+   dato: ventas;
+   sig: lista;
+ end;
+ vector = array [rango] of integer;
+
+procedure leerInfo(var v: ventas);
+begin
+ writeln('Ingrese el modelo del auto:'); readln(v.auto);
+ if (v.auto <> 'ZZZ')then begin
+    writeln('Ingrese el codigo de la veta:'); readln(v.codigo);
+    writeln('Ingrese el dni del cliente:'); readln(v.dni);
+    writeln('Ingrese el dia de la ventas (entre 1 y 28):'); readln(v.dia);
+ end;
+end;
+
+procedure insertarOrdenado(var L:ista; v:ventas);
+var
+ nue,act,ant: lista;
+begin
+ new(nue); nue^.dato:= v; ant:= L; act:= L;
+ while(act <> nil) and (v.auto > act^.dato.auto) do begin
+   ant:= act;
+   act:= act^.sig;
+ end;
+ if (act = ant) then
+   L:= nue
+ else
+   ant^.sig:= nue;
+ nue^.sig:= act;
+end;
+
+procedure CargarLista(var L: lista);
+var
+ v:ventas;
+begin
+ leerInfo(v);
+ while(v.auto <> 'ZZZ') do begin
+     insertarOrdenado(L,v);
+     leerInfo(v);
+end;
+
+procedure InicializarVector(a:vector);
+var
+ i: rango;
+begin
+ for i:= 1 to r do 
+   a[i]:= 0;
+end;
+
+function dniImpar(dni: integer):boolean;
+begin
+  dniImpar:= ((dni MOD 2) = 1);
+end;
+
+procedure maximo(a:vector; var max: integer);
+var
+ cant: integer;
+ i: rango;
+begin
+ cant:= -1;
+ for i:= 1 to r do begin
+   if (v[i] > cant)then begin
+     cant:= v[i];
+     max:= i;
+   end;
+ end;
+end;
+
+function Condicion(codigo: integer): boolean;
+begin
+ Condicion:= ((codigo MOD 2) = 0);
+end;
+
+procedure Procesar();
+{Programa Principal}
+var
+ L:lista;
+
+begin
+ L:= nil;
+ CragarLista(L);
+end.
+   
