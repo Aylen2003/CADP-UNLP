ef526493131b2ff16a37e7340c1c5141bab4d310
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-04 11:33:41 -0300

    En progreso

--- Parciales/Resoluciones/3eraFecha.pas
+++ Parciales/Resoluciones/3eraFecha.pas
@@ -30,5 +30,116 @@ type
     calificacion: arrPuntuaciones;
  end;
  lista = ^nodo;
- 
+ nodo = record
+   dato: jugador;
+   sig: lista;
+end;
+vectorEquipos = array [rangoE] of integer;
+
+procedure CargarJugadores(var L:lista); // SE DISPONE
+
+procedure Inicializar(var v:vectorEquipos);
+var
+  i: rangoE;
+begin
+  for i:= 1 to equipos do 
+     v[i]:= 0;
+end;
+
+procedure analizarPuntuaciones(a:arrPuntuaciones, var promedio: real; var fechas: integer);
+var
+ i: rangoF;
+ suma: integer;
+begin
+ suma:= 0;
+ fechas:= 0;
+ for i:= 1 to fechas do begin
+   if (v[i] <> 0) then begin
+     suma:= suma + v[i];
+     fechas:= fechas + 1;
+   end;
+ end;
+ promedio:= suma / fechas;
+end;
+
+procedure actualizarMaximo(var jug1,jug2:integer; var prom1,prom2:real;jugador: integer; promedio:real);
+begin
+  if (promedio > prom1)then begin
+     jug2:= jug1;
+     prom2:= prom1;
+     jug1:= jugador;
+     prom1:= promedio;
+     
+ end
+ else
+    if(promedio > prom2)then begin
+       prom2:= promedio;
+       jug2:= jugador;
+   end;
+end;
+
+
+procedure recorrer(L:lista; var v:vectorEquipos, var jug1,jug2: integer);
+var
+ prom,pormjug1,promjug2: real;
+ cant: integer;
+
+begin
+ promjug1:= -1;
+ promjug2:= -1;
+ while (L <>  nil) do begin
+    if(2023 - L^.dato.anioNac > 35)then
+      v[L^.dato.equipo]:= v[L^.dato.equipo] + 1;
+      
+    analizarPuntuaciones(L^.dato.calificaciones, prom, cant);
+    if (cant > 14) then 
+      actualizarMaximos(jug1,jug2,promjug1,promjug2,L^.dato.codigo,prom);
+    L:=: L^.sig;
+ end;
+end;
+
+procedure imprimir(v:vectorEquipos);
+var
+  i: rangoE;
+begin
+ for i:= 1 to equipos do 
+    writeln('El equipo', i,'tiene',v[i], 'jugadores de más de 35 anios');
+end;
+
+function contarImpares(num:integer): integer;
+var
+ cant:integer;
+begin
+  cant:= 0;
+  while(num <> 0) do begin
+    if (((num MOD 10) mod 2) = 1)then
+      cant:= cant + 1;
+    num:= num DIV 10;
+end;
+
+procedure insertarOrdenado(var L:lista; elem: jugador);
+
+
+procedure armarLista2(l:lista; var L2:lista);
+begin
+  while (L <> nil) do begin
+     if (contarImpares(L^.dato.codigo) = 3) and (L^.dato.anioNac >= 1983) and (L^.dato.anioNac <= 1990)then
+        insertarOrdenado(L2,L^.dato);
+     L:= L^.sig;
+  end;
+end;
+var 
+ L,L2:lista;
+ v: vectorEquipos;
+ jug1,jug2: integer;
+begin
+ L:= nil;
+ L2:= nil;
+ CargarJugadores(L); // SE DISPONE
+ Inicializar(v);
+ recorrer(L,v,jug1,jug2);
+ imprimir(v);
+ writeln(jug1,jug2)
+ armarLista2(L,L2);
+end;
   
