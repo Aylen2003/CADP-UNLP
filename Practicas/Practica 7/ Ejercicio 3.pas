1a1045471a8433d5acb49fce05689dadc3effa1c
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-06 12:46:10 -0300

    Actualizar Practica 7

---
+++ Practicas/Practica 7/ Ejercicio 3.pas
@@ -0,0 +1,8 @@
+{ Una remisería dispone de información acerca de los viajes realizados durante el mes de mayo de
+2020. De cada viaje se conoce: número de viaje, código de auto, dirección de origen, dirección de
+destino y kilómetros recorridos durante el viaje. Esta información **se encuentra ordenada** por código de auto y para un mismo código de auto pueden existir 1 o más viajes. Se pide:
+
+1. Informar los dos códigos de auto que más kilómetros recorrieron.
+2. Generar una lista nueva con los viajes de más de 5 kilómetros recorridos, ordenada por número
+de viaje.
+}
