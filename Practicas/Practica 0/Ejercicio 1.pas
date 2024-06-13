b3a1eabed38e20c7e928eb9cadfdf9c2d2c62c82
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-13 15:42:26 -0300

    Create Ejercicio 1.pas

---
+++ Practicas/Practica 0/Ejercicio 1.pas
@@ -0,0 +1,13 @@
+{Implemente un programa que lea por teclado dos números enteros e imprima en pantalla los valores leídos en orden inverso. Por ejemplo, 
+si se ingresan los números 4 y 8, debe mostrar el mensaje:  Se ingresaron los valores 8 y 4}
+
+program Ejer1;
+var 
+ numero:integer;
+ numero2:integer;
+begin
+	read(numero);
+	read(numero2);
+	writeln(numero2);
+	writeln(numero);
+end.
