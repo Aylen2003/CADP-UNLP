af8f904b644c72ba9f8cdff4e49777a47d4159b6
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-13 16:38:34 -0300

    Update Ejercicio 4.pas

--- Practicas/Practica 0/Ejercicio 4.pas
+++ Practicas/Practica 0/Ejercicio 4.pas
@@ -4,12 +4,43 @@
   c. El perímetro del círculo. Para calcular el perímetro del círculo debe utilizar la fórmula D*PI (o también PI*R*2).
 }
 
+// PARTE A
 program Ejer4A;
 var 
   diametro,radio:real;
 begin
-	 writeln('Dame el diametro');
-	 readln(diametro);
-	 radio:= diametro/2;
-	 writeln('Radio:',radio:5:2);
+ writeln('Dame el diametro');
+ readln(diametro);
+ radio:= diametro/2;
+ writeln('Radio:',radio:5:2);
+end.
+ 
+ // PARTE B
+ program Ejer4B;
+const 
+ pi=3.1416;
+var 
+ r:real;
+ ac:real;
+begin
+ writeln('Calcular el area del circulo');
+ write('Introduce el valor del radio:');
+ read(r);
+ ac:=pi*r*r;
+ write('El area es:',ac:5:2);
+end.
+
+// PARTE C
+program Ejer4C;
+const
+ pi=3.1416; 
+var 
+  r:real;
+  p:real;
+begin
+ writeln('Calcular el perimetro del circulo');
+ write('introduce el valor del radio:');
+ read(r);
+ p:=pi*r*2;
+ write('El perimetro es:',p:5:2);
 end.
