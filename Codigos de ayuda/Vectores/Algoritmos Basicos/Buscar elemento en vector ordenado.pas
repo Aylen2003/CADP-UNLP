66fa6905574f4f624e9e81956bcce3f384bf8e04
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-14 11:01:10 -0300

    Create Buscar elemento en vector ordenado.pas

---
+++ Codigos de ayuda/Vectores/Algoritmos Basicos/Buscar elemento en vector ordenado.pas
@@ -0,0 +1,16 @@
+// BUSCAR ELEMENTO EN VECTOR ORDENADO -> DEVUELVE LA POSICION
+
+function BuscoPosElemOrd (x: integer; v:Vector; dimL: integer): integer;
+var
+    pos : integer;
+begin
+    pos:=1;
+
+    while (pos <= dimL) and (x > v[pos]) do
+       pos:=pos+1;
+
+    if ( pos > dimL ) or (x < v [pos]) then
+       pos:=0;
+
+    BuscoPosElemOrd:= pos;
+end;
