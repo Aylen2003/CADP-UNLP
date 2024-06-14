7c15cf5be827d61794a32f8c9b51d2283751e504
Brenda Cendra <111923695+Aylen2003@users.noreply.github.com>
2024-06-14 11:02:42 -0300

    Create Buscar elemento de manera lineal.pas

---
+++ Codigos de ayuda/Vectores/Algoritmos Basicos/Buscar elemento de manera lineal.pas
@@ -0,0 +1,19 @@
+//BUSCAR ELEMENTO EN UN VECTOR -> DEVUELVE LA POSICION
+
+function BuscarPosElem (x:integer;v:vector;dimL: integer): integer;
+var
+  pos:integer;
+  exito: boolean;
+begin
+   pos:=1;
+   exito:= false;
+   while (pos <= dimL) and (not exito) do begin  //mientras la pos sea válida, y no lo encontré, avanza
+        if (x = v[pos]) then
+          exito:= true
+        else
+          pos:=pos+1; //avanza con la posicion
+   end;
+   if (exito = false) then
+     pos:=0; //devuelve pos=0 para decir que no existe
+   BuscarPosElem := pos;
+end;
